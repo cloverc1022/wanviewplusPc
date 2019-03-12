82813ebe14ecdcc2e20e033e1fa2f636892cfd1c
e <mmc5634@outlook.com>
2019-03-12 15:45:09 +0800

    模糊优化

--- app/src/main/java/net/ajcloud/wansviewplusw/camera/CameraController.java
+++ app/src/main/java/net/ajcloud/wansviewplusw/camera/CameraController.java
@@ -742,9 +742,9 @@ public class CameraController implements PoliceHelper.PoliceControlListener {
         File file = new File(FileUtil.getRealtimeImagePath(deviceId) + File.separator + "realtime_picture.jpg");
         if (file.exists()) {
             Image image = new Image(file.toURI().toString(), 0, 0, false, true, false);
-            playBg.setBackground(new Background(new BackgroundImage(image, null, null, BackgroundPosition.CENTER,
-                    new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, true, true, true, true))));
-            playBg.setEffect(new GaussianBlur());
+            playBg.setBackground(new Background(new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                    new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, true, true, true, false))));
+            playBg.setEffect(new GaussianBlur(60));
         }
     }
 
